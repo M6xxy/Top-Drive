72b316aee9ab44c3e672a92c89a653fcbd193903
Carter Morrison <40690697@live.napier.ac.uk>
2025-11-23 18:48:06 +0000

    Carter Morrison <40690697@live.napier.ac.uk>
    Added semi working car customization
    
    Signed-off-by: Carter Morrison <40690697@live.napier.ac.uk>

---
+++ header/PlayerCar.h
@@ -0,0 +1,39 @@
+//
+// Created by Carte on 23/11/2025.
+//
+
+#pragma once
+
+#include <box2d/box2d.h>
+#include <string>
+#include <SFML/Graphics.hpp>
+#include "car.h"
+#include "Movement.h"
+
+class PhysicsWorld;
+class LoadSprites;
+struct MovementIntent;
+
+class PlayerCar {
+public:
+    PlayerCar(PhysicsWorld& physics, LoadSprites& loader, const std::string& texturePath, const sf::Vector2f& startPosPixels, const CarSpec& spec);
+
+    // Update car and box2d every frame
+    void updatePhysics(const MovementIntent& intent, float dt);
+
+    // Sync sprite from box2d
+    void syncSpriteFromPhysics();
+
+    // Helpers
+    sf::Sprite& getSprite();
+    b2Body* getBody();
+    Car& getCar();
+    const Car& getCar() const;
+
+private:
+    PhysicsWorld& m_physics;
+    sf::Texture m_texture;
+    sf::Sprite m_sprite;
+    b2Body* m_body = nullptr;
+    Car m_car;
+};
