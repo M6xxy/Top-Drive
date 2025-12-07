46cc451d9c87a09dcece6b085f68331d815e52d8
M6xxy <79139144+M6xxy@users.noreply.github.com>
2025-12-07 17:24:00 +0000

    Fixes

--- cpps/Car/CarSpecController.cpp
+++ cpps/Car/CarSpecController.cpp
@@ -35,40 +35,12 @@ CarSpec CarSpecController::makeTestCarSpec() {
     // CHASSIS
     spec.chassis.name            = "Lightweight";
     spec.chassis.massKg          = 950.f;
-    spec.chassis.wheelBase      = 2.5f;
-    spec.chassis.trackFront     = 1.5f;
-    spec.chassis.trackRear      = 1.5f;
-    spec.chassis.cogHeight      = 0.45f;
-    spec.chassis.weightDistFront = 0.55f;
-
-    // SUSPENSION (placeholder)
-    spec.suspension.name            = "Sport";
-    spec.suspension.springRateFront = 30000.f;
-    spec.suspension.springRateRear  = 28000.f;
-    spec.suspension.damperFront     = 3000.f;
-    spec.suspension.damperRear      = 2800.f;
-    spec.suspension.rideHeightFront = 0.12f;
-    spec.suspension.rideHeightRear  = 0.13f;
-    spec.suspension.camberFront     = -1.5f;
-    spec.suspension.camberRear      = -1.0f;
-    spec.suspension.toeFront        = 0.1f;
-    spec.suspension.toeRear         = 0.0f;
-
-    // TYRES
-    spec.tyre.name        = "Street Tyres";
-    spec.tyre.grip        = 1.0f;
-    spec.tyre.wearRate    = 0.01f;
-    spec.tyre.tempOptimal = 80.f;
-    spec.tyre.tempRange   = 40.f;
 
     // TRANSMISSION
     spec.transmission.name            = "6-speed FWD";
     spec.transmission.numGears        = 6;
-    spec.transmission.automatic       = false;
     spec.transmission.finalDriveRatio = 4.1f;
     spec.transmission.gearRatios      = {3.3f, 2.1f, 1.5f, 1.2f, 1.0f, 0.8f};
-    spec.transmission.awdFrontTorqueSplit   = 0.5f;          // not used for FWD
-    spec.transmission.driveType       = DriveType::FWD;
 
     return spec;
 }
